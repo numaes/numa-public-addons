d581395602226b567194a64a9854112ad26cf84e
Gustavo Adrian Marino <gamarino@numaes.com>
2020-03-17 17:44:39 -0300

    [IMP] numa_imap: Grabar mensajes del servidor, en carga inicial fijar fecha de los mails

--- numa_imap/models/fetchmail.py
+++ numa_imap/models/fetchmail.py
@@ -20,8 +20,24 @@
 #
 ##############################################################################
 
+import email
+import hashlib
+import hmac
+import lxml
+import logging
+import pytz
+import re
+import socket
+import time
+import threading
+import base64
+try:
+    from xmlrpc import client as xmlrpclib
+except ImportError:
+    import xmlrpclib
+
 from odoo import models, fields, api, _
-from datetime import datetime, timedelta
+from datetime import datetime, date, timedelta
 
 import logging
 _logger=logging.getLogger(__name__)
@@ -35,6 +51,7 @@ class FetchmailServer(models.Model):
     _inherit = 'fetchmail.server'
 
     last_uid = fields.Integer('Last received UID')
+    initially_from = fields.Date('Initial load, from date')
 
     #@api.multi
     def fetch_mail(self):
@@ -44,17 +61,18 @@ class FetchmailServer(models.Model):
         }
         MailThread = self.env['mail.thread']
 
-        def initial_date():
-            return '1-Mar-2019'
+        def initial_date(fetchmail_server):
+            if fetchmail_server.initially_from:
+                from_day = fetchmail_server.initially_from
+            else:
+                from_day = datetime.now().date()
 
-            now = datetime.now()
-            last_week = now - timedelta(seconds=60 * 60 * 24 * 7)
             monthNames = {
                 1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr",
                 5: "May", 6: "Jun", 7: "Jul", 8: "Aug",
                 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"
             }
-            return f'{last_week.day}-{monthNames[last_week.month]}-{last_week.year}'
+            return f'{from_day.day}-{monthNames[from_day.month]}-{from_day.year}'
 
         for server in self:
             _logger.info('start checking for new emails on %s server %s', server.server_type, server.name)
@@ -75,7 +93,7 @@ class FetchmailServer(models.Model):
                                 selected = True
                                 result, data = imap_server.search(
                                     None,
-                                    f'(UID {(server.last_uid or 0) + 1}:* SENTSINCE {initial_date()})'
+                                    f'(UID {(server.last_uid or 0) + 1}:* SENTSINCE {initial_date(server)})'
                                 )
                                 newMsgs = sorted([int(m) for m in data[0].split() if int(m) > server.last_uid])
                                 for num in newMsgs:
@@ -128,4 +146,87 @@ class FetchmailServer(models.Model):
 class MailMessage(models.Model):
     _inherit = "mail.message"
 
-    fetchmail_server_id = fields.Many2one('fetchmail.server', "Inbound Mail Server", readonly=True, index=True, oldname='server_id')
+    fetchmail_server_id = fields.Many2one('fetchmail.server', "Inbound Mail Server", readonly=True, index=True)
+
+
+class MailThread(models.AbstractModel):
+    _inherit = 'mail.thread'
+
+    @api.model
+    def message_process(self, model, message, custom_values=None,
+                        save_original=False, strip_attachments=False,
+                        thread_id=None):
+        """ Process an incoming RFC2822 email message, relying on
+            ``mail.message.parse()`` for the parsing operation,
+            and ``message_route()`` to figure out the target model.
+
+            Once the target model is known, its ``message_new`` method
+            is called with the new message (if the thread record did not exist)
+            or its ``message_update`` method (if it did).
+
+           :param string model: the fallback model to use if the message
+               does not match any of the currently configured mail aliases
+               (may be None if a matching alias is supposed to be present)
+           :param message: source of the RFC2822 message
+           :type message: string or xmlrpclib.Binary
+           :type dict custom_values: optional dictionary of field values
+                to pass to ``message_new`` if a new record needs to be created.
+                Ignored if the thread record already exists, and also if a
+                matching mail.alias was found (aliases define their own defaults)
+           :param bool save_original: whether to keep a copy of the original
+                email source attached to the message after it is imported.
+           :param bool strip_attachments: whether to strip all attachments
+                before processing the message, in order to save some space.
+           :param int thread_id: optional ID of the record/thread from ``model``
+               to which this mail should be attached. When provided, this
+               overrides the automatic detection based on the message
+               headers.
+        """
+        # extract message bytes - we are forced to pass the message as binary because
+        # we don't know its encoding until we parse its headers and hence can't
+        # convert it to utf-8 for transport between the mailgate script and here.
+        if isinstance(message, xmlrpclib.Binary):
+            message = bytes(message.data)
+        if isinstance(message, str):
+            message = message.encode('utf-8')
+        message = email.message_from_bytes(message)
+
+        # parse the message, verify we are not in a loop by checking message_id is not duplicated
+        msg_dict = self.message_parse(message, save_original=save_original)
+        if strip_attachments:
+            msg_dict.pop('attachments', None)
+
+        existing_msg_ids = self.env['mail.message'].search([('message_id', '=', msg_dict['message_id'])], limit=1)
+        if existing_msg_ids:
+            _logger.info('Ignored mail from %s to %s with Message-Id %s: found duplicated Message-Id during processing',
+                         msg_dict.get('email_from'), msg_dict.get('to'), msg_dict.get('message_id'))
+            return False
+
+        if save_original:
+            filtered_msg = dict(
+                message_type='email',
+                message_id=msg_dict['message_id'],
+                subject=msg_dict['subject'],
+                email_from=msg_dict['email_from'],
+                email_cc=msg_dict['cc'],
+                email_to=msg_dict['recipients'],
+                references=msg_dict['references'],
+                date=msg_dict['date'],
+                state='received',
+                body=msg_dict['body'],
+                body_html=msg_dict['body'],
+            )
+            new_mail = self.env['mail.mail'].create(filtered_msg)
+            if not strip_attachments:
+                new_mail.attachment_ids = [
+                    (0, 0, {'name': a.fname,
+                            'datas': base64.b64encode(a.content) if isinstance(a.content, bytes) else
+                                     a.content}) for a in msg_dict['attachments']
+                ]
+            self.env.cr.commit()
+
+        # find possible routes for the message
+        routes = self.message_route(message, msg_dict, model, thread_id, custom_values)
+        thread_id = self._message_route_process(message, msg_dict, routes)
+        return thread_id
+
