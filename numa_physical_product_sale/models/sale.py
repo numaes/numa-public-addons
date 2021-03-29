51c4d06f80ee850b43b3a8977f769c64766a0d57
Gustavo Adrian Marino <gamarino@numaes.com>
2021-03-29 11:44:29 -0300

    [FIX] numa_physical_product_sale: raise in lines without product

--- numa_physical_product_sale/models/sale.py
+++ numa_physical_product_sale/models/sale.py
@@ -72,7 +72,7 @@ class SaleOrderLine(models.Model):
 
         # Correct values
         for line in self:
-            if not line.product:
+            if not line.product_id:
                 continue
 
             if line.product_id.type in ('product', 'consu') and line.product_id.price_base != 'normal':
