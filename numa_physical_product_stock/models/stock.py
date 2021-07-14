5eae5dd7b028c5814bf4b837e63cb84f8f9cabd3
Gustavo Adrian Marino <gamarino@numaes.com>
2021-07-14 17:22:30 -0300

    [WIP] numa_physical_product_stock: pesos y volúmenes en stock.move.line

--- numa_physical_product_stock/models/stock.py
+++ numa_physical_product_stock/models/stock.py
@@ -25,18 +25,25 @@ FIELD_NAME_PER_TYPE = {
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    picking_weight = fields.Float('Weight', compute='_compute_weight_volume')
-    picking_volume = fields.Float('Volume', compute='_compute_weight_volume')
+    picking_weight = fields.Float('Weight', compute='onchange_move_line_ids')
+    picking_volume = fields.Float('Volume', compute='onchange_move_line_ids')
 
-    @api.onchange('move_lines')
-    @api.depends('move_lines')
-    def _compute_weight_volume(self):
-        for picking in self:
-            picking.picking_weight = 0.0
-            picking.picking_volume = 0.0
-            for move in picking.move_lines:
-                picking.picking_weight += move.total_weight
-                picking.picking_volume += move.total_volume
+    @api.onchange('move_line_ids', 'move_line_ids_without_package')
+    @api.depends('move_line_ids', 'move_line_ids_without_package')
+    def onchange_move_line_ids(self):
+        for move in self:
+            picking_weight = 0.0
+            picking_volume = 0.0
+            for ml in move.move_line_ids:
+                if ml.product_id:
+                    picking_weight += ml.total_weight
+                    picking_volume += ml.total_volume
+            move.picking_weight = picking_weight
+            move.picking_volume = picking_volume
+
+    def button_validate(self):
+        self.onchange_move_line_ids()
+        return super().button_validate()
 
 
 class StockMove(models.Model):
@@ -53,20 +60,64 @@ class StockMove(models.Model):
     total_weight = fields.Float(string='Total Weight')
     total_volume = fields.Float(string='Total Volume')
 
-    @api.onchange('product_id', 'product_qty', 'product_uom_qty')
-    @api.depends('product_id', 'product_qty', 'product_uom_qty')
-    def onchange_qty(self):
+    @api.onchange('move_line_ids', 'move_line_ids_without_package')
+    @api.depends('move_line_ids', 'move_line_ids_without_package')
+    def onchange_move_line_ids(self):
         for move in self:
-            normalized_qty = move.product_uom._compute_quantity(move.product_uom_qty, move.product_id.uom_id) \
-                if move.product_uom else move.product_uom_qty
-            move.total_surface = normalized_qty * move.unit_surface
-            move.total_weight = normalized_qty * move.unit_weight
-            move.total_volume = normalized_qty * move.unit_volume
+            total_weight = 0.0
+            total_volume = 0.0
+            total_surface = 0.0
+            for ml in move.move_line_ids:
+                if ml.product_id:
+                    total_weight += ml.total_weight
+                    total_surface += ml.total_surface
+                    total_volume += ml.total_volume
+            move.total_weight = total_weight
+            move.total_surface = total_surface
+            move.total_volume = total_volume
 
     @api.model_create_multi
     def create(self, vals_list):
         new_moves = super().create(vals_list)
-        new_moves.onchange_qty()
+        new_moves.onchange_move_line_ids()
         return new_moves
+
+    def write(self, vals):
+        ret = super().write(vals)
+        if 'move_line_ids' in vals:
+            self.onchange_move_line_ids()
+        return ret
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    unit_width = fields.Float(string='Unit Width', related='product_id.product_width', readonly=True)
+    unit_length = fields.Float(string='Unit Length', related='product_id.product_length', readonly=True)
+    unit_height = fields.Float(string='Unit Height', related='product_id.product_height', readonly=True)
+    unit_surface = fields.Float(string='Unit Surface', related='product_id.surface', readonly=True)
+    unit_weight = fields.Float(string='Unit Weight', related='product_id.weight', readonly=True)
+    unit_volume = fields.Float(string='Unit Volume', related='product_id.volume', readonly=True)
+
+    total_surface = fields.Float(string='Total Surface')
+    total_weight = fields.Float(string='Total Weight')
+    total_volume = fields.Float(string='Total Volume')
+
+    @api.onchange('product_id', 'qty_done')
+    @api.depends('product_id', 'qty_done')
+    def onchange_qty(self):
+        for move_line in self:
+            normalized_qty = move_line.product_uom_id._compute_quantity(move_line.qty_done,
+                                                                        move_line.product_id.uom_id) \
+                if move_line.product_uom_id else move_line.product_uom_qty
+            move_line.total_surface = normalized_qty * move_line.unit_surface
+            move_line.total_weight = normalized_qty * move_line.unit_weight
+            move_line.total_volume = normalized_qty * move_line.unit_volume
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        new_move_lines = super().create(vals_list)
+        new_move_lines.onchange_qty()
+        return new_move_lines
 
 
