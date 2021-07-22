6539c20152564519077120671993150f65d0de2e
Gustavo Adrian Marino <gamarino@numaes.com>
2021-07-22 17:06:14 -0300

    [FIX] numa_physical_product_sale: do not change price unit on update

--- numa_physical_product_sale/models/sale.py
+++ numa_physical_product_sale/models/sale.py
@@ -233,3 +233,49 @@ class SaleOrderLine(models.Model):
         else:
             return self.qty_to_invoice, self.qty_to_invoice
 
+    @api.model
+    def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount,
+                                            currency, product, partner, taxes, move_type):
+        ''' This method is used to compute 'price_total' & 'price_subtotal'.
+
+        :param price_unit:  The current price unit.
+        :param quantity:    The current quantity.
+        :param discount:    The current discount.
+        :param currency:    The line's currency.
+        :param product:     The line's product.
+        :param partner:     The line's partner.
+        :param taxes:       The applied taxes.
+        :param move_type:   The type of the move.
+        :return:            A dictionary containing 'price_subtotal' & 'price_total'.
+        '''
+        res = {}
+
+        # Compute 'price_subtotal'.
+        line_discount_price_unit = price_unit * (1 - (discount / 100.0))
+        subtotal = self.price_qty * line_discount_price_unit
+
+        # Compute 'price_total'.
+        if taxes:
+            force_sign = -1 if move_type in ('out_invoice', 'in_refund', 'out_receipt') else 1
+            taxes_res = taxes._origin.with_context(force_sign=force_sign).compute_all(
+                line_discount_price_unit,
+                quantity=self.price_qty,
+                currency=currency,
+                product=product,
+                partner=partner,
+                is_refund=move_type in ('out_refund', 'in_refund'))
+            res['price_subtotal'] = taxes_res['total_excluded']
+            res['price_total'] = taxes_res['total_included']
+        else:
+            res['price_total'] = res['price_subtotal'] = subtotal
+
+        # In case of multi currency, round before it's use for computing debit credit
+        if currency:
+            res = {k: currency.round(v) for k, v in res.items()}
+        return res
+
+    @api.model
+    def _get_fields_onchange_balance_model(self, quantity, discount, amount_currency, move_type, currency, taxes, price_subtotal, force_computation=False):
+        return {}
+
+
