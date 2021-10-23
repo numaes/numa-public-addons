33ef95260384a7c6d8f264f0f693133a18c6d981
Gustavo Adrian Marino <gamarino@numaes.com>
2021-10-23 18:09:55 -0300

    [FIX] numa_physical_product_stock: transporte de pesada entre transferencias

--- numa_physical_product_stock/models/stock.py
+++ numa_physical_product_stock/models/stock.py
@@ -90,6 +90,83 @@ class StockMove(models.Model):
             self.onchange_move_line_ids()
         return ret
 
+    def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
+        move_line_model = self.env['stock.move.line']
+
+        result = super()._prepare_move_line_vals(quantity=quantity, reserved_quant=reserved_quant)
+
+        last_ingress = move_line_model.search([
+            ('product_id', '=', self.product_id.id),
+            ('location_dest_id', '=', self.location_id.id),
+            ('state', '=', 'done'),
+        ], order='write_date desc', limit=1)
+        if last_ingress:
+            unit_weight = last_ingress.unit_weight
+            unit_surface = last_ingress.unit_surface
+            unit_volume = last_ingress.unit_volume
+        else:
+            unit_weight = self.unit_weight
+            unit_surface = self.unit_surface
+            unit_volume = self.unit_volume
+
+        result['unit_weight'] = unit_weight
+        result['unit_surface'] = unit_surface
+        result['unit_volume'] = unit_volume
+
+        return result
+
+    def _action_assign(self):
+        move_line_model = self.env['stock.move.line']
+
+        result = super()._action_assign()
+
+        for move in self:
+            for move_line in move.move_line_ids:
+                last_ingress = move_line_model.search([
+                    ('product_id', '=', move_line.product_id.id),
+                    ('location_dest_id', '=', move_line.location_id.id),
+                    ('picking_id.state', '=', 'done'),
+                ], order='write_date desc', limit=1)
+                if last_ingress:
+                    move_line.write({
+                        'unit_weight': last_ingress.unit_weight,
+                        'unit_surface': last_ingress.unit_surface,
+                        'unit_volume': last_ingress.unit_volume
+                    })
+                else:
+                    move_line.write({
+                        'unit_weight': move_line.product_id.weight,
+                        'unit_surface': move_line.product_id.surface,
+                        'unit_volume': move_line.product_id.volume
+                    })
+                move_line.flush()
+                move_line.onchange_qty()
+
+        return result
+
+    def _action_done(self, cancel_backorder=False):
+        move_line_model = self.env['stock.move.line']
+
+        result = super()._action_done()
+
+        for move in self:
+            for move_line in move.move_line_ids:
+                all_reserved = move_line_model.search([
+                    ('product_id', '=', move_line.product_id.id),
+                    ('location_id', '=', move_line.location_dest_id.id),
+                    ('picking_id.state', 'in', ['assigned']),
+                ])
+                if all_reserved:
+                    all_reserved.write({
+                        'unit_weight': move_line.unit_weight,
+                        'unit_surface': move_line.unit_surface,
+                        'unit_volume': move_line.unit_volume
+                    })
+                    all_reserved.flush()
+                    all_reserved.onchange_qty()
+
+        return result
+
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
@@ -97,9 +174,9 @@ class StockMoveLine(models.Model):
     unit_width = fields.Float(string='Unit Width', related='product_id.product_width', readonly=True)
     unit_length = fields.Float(string='Unit Length', related='product_id.product_length', readonly=True)
     unit_height = fields.Float(string='Unit Height', related='product_id.product_height', readonly=True)
-    unit_surface = fields.Float(string='Unit Surface', related='product_id.surface', readonly=True)
-    unit_weight = fields.Float(string='Unit Weight', related='product_id.weight', readonly=True)
-    unit_volume = fields.Float(string='Unit Volume', related='product_id.volume', readonly=True)
+    unit_surface = fields.Float(string='Unit Surface')
+    unit_weight = fields.Float(string='Unit Weight')
+    unit_volume = fields.Float(string='Unit Volume')
 
     total_surface = fields.Float(string='Total Surface')
     total_weight = fields.Float(string='Total Weight')
@@ -111,15 +188,32 @@ class StockMoveLine(models.Model):
         for move_line in self:
             normalized_qty = move_line.product_uom_id._compute_quantity(move_line.qty_done,
                                                                         move_line.product_id.uom_id) \
-                if move_line.product_uom_id else move_line.product_uom_qty
+                if move_line.product_uom_id else move_line.qty_done
             move_line.total_surface = normalized_qty * move_line.unit_surface
             move_line.total_weight = normalized_qty * move_line.unit_weight
             move_line.total_volume = normalized_qty * move_line.unit_volume
+
+    @api.onchange('total_weight', 'total_surface', 'total_volume', 'qty_done')
+    @api.depends('total_weight', 'total_surface', 'total_volume', 'qty_done')
+    def onchange_total_physicals(self):
+        for move_line in self:
+            normalized_qty = move_line.product_uom_id._compute_quantity(move_line.qty_done,
+                                                                        move_line.product_id.uom_id) \
+                if move_line.product_uom_id else move_line.qty_done
+            if normalized_qty:
+                if move_line.unit_weight != move_line.total_weight / normalized_qty:
+                    move_line.unit_weight = move_line.total_weight / normalized_qty
+                if move_line.unit_surface != move_line.total_surface / normalized_qty:
+                    move_line.unit_surface = move_line.total_surface / normalized_qty
+                if move_line.unit_volume != move_line.total_volume / normalized_qty:
+                    move_line.unit_volume = move_line.total_volume / normalized_qty
+            else:
+                move_line.unit_weight = 0
+                move_line.unit_surface = 0
+                move_line.unit_volume = 0
 
     @api.model_create_multi
     def create(self, vals_list):
         new_move_lines = super().create(vals_list)
         new_move_lines.onchange_qty()
         return new_move_lines
-
-
