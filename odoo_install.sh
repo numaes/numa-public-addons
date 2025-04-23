bd3c81f971c60a9fe154cf37db4d374fe50774b9
Gustavo Marino <gamarino@numaes.com>
2025-04-23 20:20:17 -0300

    [FIX] requirements.txt, odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -419,8 +419,16 @@ fi
 # Install Nginx if needed
 #--------------------------------------------------
 if [ "$INSTALL_NGINX" = "True" ]; then
-  echo -e "\n---- Installing and setting up Nginx ----"
-  sudo apt install nginx letsencrypt -y
+    # Mejora: Instalar letsencrypt-nginx si aún no lo está
+    sudo apt install python3-pip python3-venv python3-wheel python3-dev libxslt1-dev libzip-dev libldap2-dev libsasl2-dev libssl-dev libffi-dev libxml2-dev libxmlsec1-dev build-essential wget git nodejs npm rtlcss libjpeg-dev zlib1g-dev -y;
+    sudo apt install python-rlPyCairo
+    sudo apt install certbot python3-certbot-nginx
+    sudo letsencrypt certonly --nginx -d "$WEBSITE_NAME" --noninteractive --agree-tos --email "$ADMIN_EMAIL" --redirect
+    sudo service nginx reload
+    echo "SSL/HTTPS is enabled!"
+else
+  echo "SSL/HTTPS isn't enabled due to choice of the user or because of a misconfiguration!"
+fi
 
   cat <<EOF > ~/odoo
 #odoo server
@@ -627,16 +635,6 @@ fi
 # Enable ssl with certbot
 #--------------------------------------------------
 
-if [ "$INSTALL_NGINX" = "True" ] && [ "$ADMIN_EMAIL" != "odoo@example.com" ] && [ "$WEBSITE_NAME" != "_" ]; then
-    # Mejora: Instalar letsencrypt-nginx si aún no lo está
-    sudo apt install python3-pip python3-venv python3-wheel python3-dev libxslt1-dev libzip-dev libldap2-dev libsasl2-dev libssl-dev libffi-dev libxml2-dev libxmlsec1-dev build-essential wget git nodejs npm rtlcss libjpeg-dev zlib1g-dev -y;
-    sudo apt install python-rlPyCairo letsencrypt letsencrypt-nginx
-    sudo letsencrypt certonly --standalone -d "$WEBSITE_NAME" --noninteractive --agree-tos --email "$ADMIN_EMAIL" --redirect
-    sudo service nginx reload
-    echo "SSL/HTTPS is enabled!"
-else
-  echo "SSL/HTTPS isn't enabled due to choice of the user or because of a misconfiguration!"
-fi
 
 echo "-----------------------------------------------------------"
 echo "Done!. Specifications:"
